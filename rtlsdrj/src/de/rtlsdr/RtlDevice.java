a6cf13cce3fd87d8e083c7855114a62b7a37706b
r.schoch <r.schoch@t-online.de>
2013-10-03 21:14:24 +0000

    added GPL header comment & correction in some return types of RtlDevice

--- rtlsdrj/src/de/rtlsdr/RtlDevice.java
+++ rtlsdrj/src/de/rtlsdr/RtlDevice.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2013 by Robert Schoch <r.schoch@t-online.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.rtlsdr;
 
 import java.nio.ByteBuffer;
@@ -68,13 +85,13 @@ public class RtlDevice {
   public native int getTunerGain();
   
   public native void setTunerIfGain(int stage, int gain);
-  public native int setTunerGainMode(int manual);
+  public native void setTunerGainMode(int manual);
   
   public native void setSampleRate(int rate);
   public native int getSampleRate();
   
   public native void setTestMode(int on);
-  public native int setAgcMode(int on);
+  public native void setAgcMode(int on);
   
   public native void setDirectSampling(int on);
   public native int getDirectSampling();
